912752d1fe6fb54e366c83dcc2f56ba27a9b4cd4
Akmalov Dilmurod <dilmurodakmalov20@gmail.com>
2026-01-16 21:12:48 +0500

    ...

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,29 @@
 'use client' 
 
+import logo from '@/public/logo.svg'
+import Image from 'next/image'
+
 const Navbar: React.FC = () => {
 
-
     return (
-        <div className="max-w-7xl mx-auto">
-
+        <div className="max-w-7xl mx-auto px-4 md:px-8">
+            <div className="flex items-center justify-between h-16 md:h-20"></div>
+            {/* Logo and name */}
+            <div className="flex justify-end items-center">
+                <Image
+                src="/public/logo.svg" 
+                alt="Logo" 
+                width={24} 
+                height={24}  
+                className="object-contain"
+                />
+            </div>
+            <div className='text-[#1A1615] text-xl font-bold '>
+                Dreelio
+            </div>
+            <div className='lg:flex items-center gap-8'></div>
         </div>
     )
 }
+
+export default Navbar 
