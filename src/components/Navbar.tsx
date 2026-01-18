ccb9c38a7200164f9f74d0d9e3ff6e423e6d168f
Akmalov Dilmurod <dilmurodakmalov20@gmail.com>
2026-01-18 23:12:30 +0500

    garbaage commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,19 +7,25 @@ const Navbar: React.FC = () => {
 
     return (
         <div className="max-w-7xl mx-auto px-4 md:px-8">
-            <div className="flex items-center justify-between h-16 md:h-20"></div>
+            <div className="flex items-center justify-around h-16 md:h-20"></div>
             {/* Logo and name */}
-            <div className="flex justify-end items-center">
-                <Image
-                src="/public/logo.svg" 
-                alt="Logo" 
-                width={24} 
-                height={24}  
-                className="object-contain"
-                />
+            <div className='flex items-center justify-between'>
+                <div className="flex justify-end items-center">
+                    <Image
+                    src="/public/logo.svg" 
+                    alt="Logo" 
+                    width={24} 
+                    height={24}  
+                    className="object-contain"
+                    />
+                </div>
+                <div className='text-[#1A1615] text-xl font-bold'>
+                    Dreelio
+                </div>
             </div>
-            <div className='text-[#1A1615] text-xl font-bold '>
-                Dreelio
+            {/* Navigation */}
+            <div className='flex items-center justify-around max-w-2xl mx-auto'>
+                
             </div>
             <div className='lg:flex items-center gap-8'></div>
         </div>
