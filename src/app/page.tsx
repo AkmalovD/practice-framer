ebc458cd69cec0ad32c7b6583d37d6e543a81193
Akmalov Dilmurod <dilmurodakmalov20@gmail.com>
2026-02-04 20:51:02 +0500

    remake of Hero.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,10 @@ import Navbar from "../components/Navbar"
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <Hero />
+      <div className="min-h-screen bg-gradient-to-b from-[#C5DBF0] via-[#E8F1F8] to-[#D8E8F4]">
+        <Navbar />
+        <Hero />
+      </div>
     </>
   )
 } 
